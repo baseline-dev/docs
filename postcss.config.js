407f86365e0edd2b4dd5d354a3ff5edfa4c105a9
Nikolai Onken <nikolai@nikolaionken.com>
2020-03-31 23:49:35 +0200

    Initial checkin with basic scaffolding.

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './templates/**/*.njk',
+    './templates/**/*.html'
+  ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+  ]
+};
